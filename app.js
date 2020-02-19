4b744f8612716b1f85a077a6ae9dbc5995a7a1ca
Daylin <daylinfeina@gmail.com>
2020-02-20 00:21:51 +0100

    Implementacion listar

--- app.js
+++ app.js
@@ -1,25 +1,66 @@
-$(function(){
+$(function () {
     console.log("jquery funcionando");
     $('#task-result').hide();
-    $('#search').keyup(function (e){
-        if($('#search').val()){
-      
+    fetchTaks();
+
+    $('#search').keyup(function (e) {
+        if ($('#search').val()) {
+
             let search = $('#search').val();
             $.ajax({
-                url :'task-search.php',
-                type :'POST',
-                data :{search},
-                success: function(response){
+                url: 'task-search.php',
+                type: 'POST',
+                data: { search },
+                success: function (response) {
                     let tasks = JSON.parse(response);
                     //console.log(tasks);
                     let templete = '';
                     tasks.forEach(task => {
                         templete += `<li>${task.name}</li>`;
                     });
-                    $('#container').html(templete); 
+                    $('#container').html(templete);
                     $('#task-result').show();
-                } 
+                }
             })
-        }    
+        }
     })
+    //formulario
+    $('#task-form').submit(function (e) {
+        const postData = {
+            name: $('#name').val(),
+            description: $('#description').val()
+        }
+        //console.log(postData);
+        //enviar datos al servidor 
+        $.post('task-add.php', postData, function (response) {
+            console.log(response);
+            fetchTaks();
+            $('#task-form').trigger('reset');
+        });
+        e.preventDefault();
+    });
+
+    function fetchTaks() {
+        $.ajax({
+            url: 'task-list.php',
+            type: 'GET',
+            success: function (response) {
+                //console.log(response);
+                let task = JSON.parse(response);
+                let template = '';
+                task.forEach(task => {
+                    template += `
+                        <tr>
+                            <td>${task.id}</td>
+                            <td>${task.name}</td>
+                            <td>${task.description}</td>
+                            <td><button class='btn btn-danger'>Delete</button></td>
+                        </tr>
+                    `;
+                });
+                $('#tasks').html(template);
+            }
+        });
+    }
+
 });
