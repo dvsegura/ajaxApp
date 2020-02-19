117c47d2e5606afcb897ef938240d995527b8e0d
Daylin <daylinfeina@gmail.com>
2020-02-20 00:22:41 +0100

    implementar listar

--- task-add.php
+++ task-add.php
@@ -1,4 +1,17 @@
 <?php 
-
+    include('database.php');
+    if(isset($_POST['name'])){
+        $name = $_POST['name'];
+        $description = $_POST['description'];
+        $query = "INSERT INTO task 
+                    (name,description) 
+                    VALUES('$name','$description')
+            ";
+        $result = mysqli_query($conection,$query);
+        if(!$result){
+            die("Query Failed");
+        }
+        echo "Tarea efectuada correctamente";
+    }
  
  ?>
---
+++ task-list.php
@@ -0,0 +1,19 @@
+<?php 
+    include('database.php');
+    $query = "SELECT * from task";
+    $result = mysqli_query($conection,$query);
+
+    if(!$result){
+        die('Query Failed'.mysqli_error($conection));
+    }
+    $json = array();
+    while($row = mysqli_fetch_array($result)){
+        $json [] = array(
+            'name' => $row['name'],
+            'description' => $row['description'],
+            'id' => $row['id']
+        ); 
+    }
+    $jsonstring = json_encode($json);
+    echo $jsonstring;
+?>
